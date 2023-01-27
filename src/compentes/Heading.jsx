ffb960c9c681badd30087f98567b0cb1d9f0d268
Safouk <57320743+Safouk@users.noreply.github.com>
2023-01-27 17:16:25 +0100

    Initial commit

---
+++ src/compentes/Heading.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My favorite Drings</h1>;
+}
+export default Heading;
---
+++ src/compentes/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Kaffe</li>
+      <li>Tee</li>
+      <li>Saft</li>
+    </ul>
+  );
+}
+export default List;
